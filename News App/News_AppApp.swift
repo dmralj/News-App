1e90f3043f0c1af245d7a6f13bc9fe1a4548692b
dmralj <dev-dnh.jlb@proton.me>
2024-02-21 08:41:48 +0300

    Initial Commit

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by dvp on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
